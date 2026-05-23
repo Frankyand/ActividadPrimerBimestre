d1636fd22d0223cc52d76198c79cac6ca8dbded0
Frankyand <frankerney996@gmail.com>
2026-05-23 17:36:03 -0500

    Primer commit actividad

---
+++ app/src/main/java/com/example/actividadprimerbimestre/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.actividadprimerbimestre
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val boton = findViewById<Button>(R.id.btInicio)
+
+        boton.setOnClickListener {
+
+            val intent = Intent(this, SegundaPantalla::class.java)
+            startActivity(intent)
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/actividadprimerbimestre/SegundaPantalla.kt
@@ -0,0 +1,21 @@
+package com.example.actividadprimerbimestre
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class SegundaPantalla : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_segunda_pantalla)
+
+        val btnRegresar = findViewById<Button>(R.id.button)
+
+        btnRegresar.setOnClickListener {
+            finish()
+        }
+    }
+}
